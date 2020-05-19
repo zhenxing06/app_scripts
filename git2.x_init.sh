58de80103bce8c9ca3dc8b9519a6c520e4fc349d
fengmanlou <zhenxing_06@163.com>
2020-05-19 11:29:54 +0800

    Update git2.x_init.sh
    Add Git2.x Build Method

--- git2.x_init.sh
+++ git2.x_init.sh
@@ -2,7 +2,23 @@
 
 
 
+functions yum_git(){
+  curl https://setup.ius.io | sh
+  yum remove -y git | yum -y install git2u
+  git --version
+}
 
-curl https://setup.ius.io | sh
-yum remove -y git | yum -y install git2u
-git --version
+functions tar_git(){
+   yun remove git
+   yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel 
+   yum install gcc perl-ExtUtils-MakeMaker
+   cd /tmp
+   wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.22.0.tar.gz
+   tar xzf git-2.22.0.tar.gz
+   cd git-2.22.0
+   make prefix=/usr/local/git all
+   make prefix=/usr/local/git install
+   echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+   source /etc/bashrc
+   git --version
+}
