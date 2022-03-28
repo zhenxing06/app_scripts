f99b77f3f0de55c8a00143c13b8fb450e179d65d
fengmanlou <zhenxing_06@163.com>
2022-03-28 18:18:31 +0800

    Update c7_init.sh
    change  centos 7 yum  repo

--- c7_init.sh
+++ c7_init.sh
@@ -43,7 +43,9 @@ ServerEnable() {
 ConfigSource(){
         yum install wget curl epel* -y
         cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-        wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+	curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+
+        #wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
         cp /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.bak
         wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
