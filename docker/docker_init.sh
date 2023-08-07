9667b4a54417de71d1be0d0709e3c965268cf2cb
fengmanlou <zhenxing_06@163.com>
2023-08-07 10:20:42 +0800

    Update docker_init.sh
    change docker-compose download url

--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -49,7 +49,9 @@ systemctl restart docker
 
 ###  安装docker-compose
 ```bash
-sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
